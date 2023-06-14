67dec7749887da03a1d8af14e45f3d5d73827565
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-14 15:35:28 +0300

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const pathFile = path.join(__dirname, 'files/fresh.txt');
+    const contentFile = 'I am fresh and young';
+    const errorText = 'FS operation failed';
+    try {
+        fs.access(pathFile, function(error) {
+            if(error == null) {
+
+                throw new Error(errorText);
+            } else if(error.code == 'ENOENT') {
+                fs.appendFile(pathFile, contentFile, () => {});
+            }
+        });
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 await create();
