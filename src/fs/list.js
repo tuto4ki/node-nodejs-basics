d317c897591e86401c85bec24424e761b2ad94d2
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-14 22:43:23 +0300

    feat: list files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const dirName =  path.join(__dirname, 'files');
+    const errorMessage = 'FS operation failed';
+
+    fs.readdir(dirName,
+        (error, items) => {
+            if (error?.code === 'ENOENT') {
+                throw new Error(errorMessage);
+            }
+            for (let i = 0; i < items.length; i++) {
+                process.stdout.write(`${items[i]}\n`);
+            }
+        }
+    );
 };
 
 await list();
