17c2379b78bd2eea26d08a94e42130e0a6bc8f85
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-15 00:02:37 +0300

    feat: read file (stream)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const fileName = path.join(__dirname, 'files/fileToRead.txt');
+    const readStream = createReadStream(fileName, 'utf-8');
+    
+    await pipeline(readStream, process.stdout);
 };
 
 await read();
