9451f31b0d9e1fde01ea855d9b8976ce589efb87
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-15 00:19:26 +0300

    feat: write in file(stream)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const fileName = path.join(__dirname, 'files/fileToWrite.txt');
+    const readStream = createWriteStream(fileName, 'utf-8');
+    
+    await pipeline(process.stdin, readStream);
 };
 
 await write();
