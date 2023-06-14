ff2f424de93b8b1a66e384ff51dd20f1b8f3b5f0
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-14 23:09:34 +0300

    feat: read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import { readFile } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const fileName = path.join(__dirname, 'files/fileToRead.txt');
+    
+    const errorMessage = 'FS operation failed';
+
+    readFile(fileName, 'utf8', (err, data) => {
+        if(err) {
+            if (err.code === 'ENOENT') {
+                throw new Error(errorMessage);
+            }
+            throw err;
+        }
+
+        console.log(data);
+    });
 };
 
 await read();
