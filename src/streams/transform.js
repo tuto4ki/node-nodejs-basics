8e54b971f387a288305a245876a523ac9881db57
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-15 00:53:56 +0300

    feat: transform enter string

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    const upperCaseTr = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join('') + '\n');
+        }
+    });
+
+    await pipeline(process.stdin, upperCaseTr, process.stdout);
 };
 
 await transform();
