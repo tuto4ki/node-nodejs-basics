c65ec1d8c1abe5f5d6caecd423399e4a0749884d
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-14 18:16:40 +0300

    feat: delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { unlink, promises } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filesName = path.join(__dirname, 'files/fileToRemove copy.txt');
+
+    const errorMessage = 'FS operation failed';
+
+    try {
+        unlink(filesName, (err) => {
+            if (err) {
+                throw new Error(errorMessage);
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await remove();
