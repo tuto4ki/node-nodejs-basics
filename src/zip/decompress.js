6c7ef65b1e877c10a1b7b6a99fd217feef715042
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-15 22:37:14 +0300

    feat: file decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const pathFile = path.join(__dirname, 'files/fileToCompress.txt');
+    const pathGzip = path.join(__dirname, 'files/archive.gz');
+
+    const unzip = createUnzip();
+    const source = createReadStream(pathGzip);
+    const destination = createWriteStream(pathFile);
+
+    pipeline(source, unzip, destination, (err) => err && console.error(err));
 };
 
 await decompress();
