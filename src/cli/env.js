ef776625abd29e430fc383e68f86e7a085f588d5
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-15 14:51:55 +0300

    feat: print environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    for(const valueEnv in process.env) {
+        if(valueEnv.search(/^RSS_/) === 0) {
+            console.log(`${valueEnv}=${process.env[valueEnv]}`);
+        }
+    }
 };
 
 parseEnv();
