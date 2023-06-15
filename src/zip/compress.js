be49c20374477fde2b23128e916f9b9c712012fe
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-15 22:19:23 +0300

    feat: file compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const pathFile = path.join(__dirname, 'files/fileToCompress.txt');
+    const pathGzip = path.join(__dirname, 'files/archive.gz');
+
+    const gzip = createGzip();
+    const source = createReadStream(pathFile);
+    const destination = createWriteStream(pathGzip);
+
+    pipeline(source, gzip, destination, (err) => err && console.error(err));
 };
 
 await compress();
