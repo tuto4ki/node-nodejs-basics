75314e2f1a63d8929a7507adf331fd07d650b42f
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-17 00:49:16 +0300

    feat: add worker

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,10 +1,9 @@
 import { createHash } from 'crypto';
 import { createReadStream } from 'fs';
 import { stdout } from 'process';
-import path from 'path';
-
-import { fileURLToPath } from 'url';
 import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,19 +13,9 @@ const calculateHash = async () => {
 
     const pathFile = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
     const input = createReadStream(pathFile);
-    
+
     await pipeline(input, hash);
     stdout.write(hash.digest('hex'));
-    /*
-    input.on('readable', () => {
-        const data = input.read();
-        if (data)
-          hash.update(data);
-        else {
-            stdout.write(hash.digest('hex'));
-        }
-      });
-    */
 };
 
 await calculateHash();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const pathFile = path.join(__dirname, 'worker.js');
+    const numCPUs = cpus().length;
+    
+    const funcWorker = (number) => {
+        return new Promise((resolve, reject) => {
+          const worker = new Worker(pathFile, {
+            workerData: number,
+          });
+
+          worker.on('message', (data) => resolve({ status: 'resolved', data }));
+          worker.on('error', () => reject({ status: 'error', data: null }));
+          worker.on('exit', (code) => {
+            if (code !== 0)
+              reject(new Error(`Worker stopped with exit code ${code}`));
+          });
+        });
+      };
+    
+      const resultArray = [];
+
+    for (let i = 0; i < numCPUs; i++) {
+        const number = 10 + i;
+        resultArray.push(funcWorker(number));
+    }
+
+    const resultPromise = await Promise.allSettled(resultArray);
+    const resultWorker = resultPromise.map((valuePromise) => valuePromise.value);
+
+    console.log(resultWorker);
 };
 
 await performCalculations();
