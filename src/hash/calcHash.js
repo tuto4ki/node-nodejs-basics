2926973312778b66250842932849f70f4aed3c74
Svetlana Safonova <safonova.sv.a@gmail.com>
2023-06-16 17:18:17 +0300

    feat: file read changed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,20 +1,32 @@
 import { createHash } from 'crypto';
 import { createReadStream } from 'fs';
 import { stdout } from 'process';
-import { pipeline } from 'stream/promises';
 import path from 'path';
 
 import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const calculateHash = async () => {
     const hash = createHash('sha256');
-    const pathFile = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
 
+    const pathFile = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
     const input = createReadStream(pathFile);
-    await pipeline(input, hash, hash.digest('hex'), stdout);
+    
+    await pipeline(input, hash);
+    stdout.write(hash.digest('hex'));
+    /*
+    input.on('readable', () => {
+        const data = input.read();
+        if (data)
+          hash.update(data);
+        else {
+            stdout.write(hash.digest('hex'));
+        }
+      });
+    */
 };
 
 await calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,15 +1,55 @@
-import { createReadStream } from 'fs';
+import { Readable } from 'stream';
 import { pipeline } from 'stream/promises';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const read = async () => {
     const fileName = path.join(__dirname, 'files/fileToRead.txt');
-    const readStream = createReadStream(fileName, 'utf-8');
-    
-    await pipeline(readStream, process.stdout);
+    class MyReadable extends Readable {
+        constructor(fileName, encoding) {
+          super();
+          this.fileName = fileName;
+          this.fileDescriptor = null;
+          this.setEncoding = encoding;
+        }
+
+        _construct(callback) {
+            fs.open(this.fileName, (error, fileDescriptor) => {
+                if (error) {
+                    callback(error);
+                } else {
+                    this.fileDescriptor = fileDescriptor;
+                    callback();
+                }
+            });
+        }
+       
+        _read(size) {
+            const buffer = Buffer.alloc(size);
+            fs.read(this.fileDescriptor, buffer, 0, size, null, (error, bytes) => {
+                if (error) {
+                    this.destroy(error);
+                } else {
+                    this.push(bytes ? buffer : null);
+                }
+            });
+        }
+        
+        _destroy(error, callback) {
+            if (this.fileDescriptor) {
+                fs.close(this.fileDescriptor, (errorClose) => error ? callback(error) : callback(errorClose));
+            } else {
+                callback(error);
+            }
+        }
+      }
+       
+      const readable = new MyReadable(fileName, 'utf8');
+
+      pipeline(readable, process.stdout);
 };
 
 await read();
